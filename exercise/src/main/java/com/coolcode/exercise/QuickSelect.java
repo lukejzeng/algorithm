9706ae58765a1dc21dab7f4d4258b78f5a1907b2
Luke <Luke@LAPTOP-H8PUO6FL>
2018-10-10 08:58:38 -0400

    Started quick select

---
+++ exercise/src/main/java/com/coolcode/exercise/QuickSelect.java
@@ -0,0 +1,20 @@
+package com.coolcode.exercise;
+
+public class QuickSelect 
+{
+	private int [] nums;
+	private boolean isLargest = true;
+	private int kth;
+	
+	public int select()
+	{
+		return partition(0, nums.length);
+	}
+		
+	public int partition(int start, int end)
+	{
+		int pivot = 0;
+		return pivot;
+	}
+		
+}
