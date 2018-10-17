609e2fdcdaf8cd1514040ce8ebc069503724d45b
elujzen <elujzen@US00205243.ericsson.se>
2018-10-17 08:19:00 -0400

    added util class

---
+++ exercise/src/main/java/com/coolcode/exercise/common/util.java
@@ -0,0 +1,18 @@
+package com.coolcode.exercise.common;
+
+public class util {
+
+	public static void swap(int [] arr, int idx1, int idx2)
+	{
+		int tmp = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = tmp;
+		return;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
