e22f0a8da5d046b02e4e1ab99189d90057eab284
Luke <lukejzeng@gmail.com>
2018-10-16 21:15:45 -0400

    N Queen problem

---
+++ exercise/src/main/java/com/coolcode/exercise/backtrack/nqueens/App.java
@@ -0,0 +1,14 @@
+package com.coolcode.exercise.backtrack.nqueens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	NQueens nq = new NQueens(4);
+    	nq.solve();
+    }
+}
