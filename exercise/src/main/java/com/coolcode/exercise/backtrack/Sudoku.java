fdcaffa1f173def9c5162163ec1e27e8e411d1cb
Luke <Luke@Kingdom>
2018-11-27 19:58:00 -0500

    new algorithm

--- exercise/src/main/java/com/coolcode/exercise/backtrack/Sudoku.java
+++ exercise/src/main/java/com/coolcode/exercise/backtrack/Sudoku.java
@@ -1,21 +1,59 @@
 package com.coolcode.exercise.backtrack;
 
+import java.util.List;
+
 public class Sudoku {
 	
 	private int [][] matrix;
 	
-	private int numberOfBlanks;
+	private int emptyCellIndex;
+	private int rowIndex;
+	private int columnIndex;
 	
+	private List<Pair<Integer>> emptyCells;
+
+	static public class Pair <T>
+	{
+		Pair(T first, T second)
+		{
+			this.first = first;
+			this.second = second;
+		}
+		T first;
+		T second;
+	}
+
 	Sudoku(int [][] matrix)
 	{
 		this.matrix = matrix; 
+		for(int i = 0; i<matrix.length; i++)
+			for(int j = 0; j<matrix[0].length; j++)
+			{
+				if(matrix[i][j] == 0) 
+					emptyCells.add(new Pair(new Integer(i), new Integer(j)));
+			}
 	}
 	
-	public boolean solve(int startX, int startY)
+	public boolean solve(int nextCellIndex)
 	{
+		if(isFinished()) return true;
+		Pair<Integer> cell = emptyCells.get(nextCellIndex);
+		for(int value = 0; value < matrix.length; value++)
+		{
+			if(isValidValue(cell.first, cell.second, value))
 		return false;
 	}
-	
+		
+	private boolean isNumberExistedInSquare(int row, int column, int value)
+	{
+		for(int i = row; i< row + 3; i++)
+		{
+			if (binarySearch(column, column + 2, matrix[i], value))
+					return true;
+		}
+		return false;
+	}
+
 	private boolean isNumberExisted(int [] row, int value)
 	{
 		return binarySearch(0, row.length, row, value);
@@ -23,10 +61,8 @@ public class Sudoku {
 	
 	public static boolean binarySearch(int start, int end, int [] arr, int value)
 	{
-		if(arr[start] == value) return true;
-		if(arr[end] == value) return true;
-		if(end - start < 1)
-			return arr[start] == value;
+		if(arr[start] == value || arr[end] == value) return true;
+		if(end - start < 1) return arr[start] == value;
 		
 		int mid = start + (end - start)/2; 
 		if( binarySearch(start, mid, arr, value) == true)
@@ -38,7 +74,7 @@ public class Sudoku {
 
 	public boolean isFinished()
 	{
-		return numberOfBlanks == 0;
+		return emptyCellIndex == emptyCells.size()-1;
 	}
 	
 	public void printMatrix()
@@ -68,7 +104,20 @@ public class Sudoku {
 
 	public static void main(String[] args) 
 	{
-		testBinarySearch();
+				
+		int [][] matrix = {{5,3,0,0,7,0,0,0,0}, 
+						   {6,0,0,1,9,5,0,0,0}, 
+						   {0,9,8,0,0,0,0,6,0}, 
+						   {8,0,0,0,6,0,0,0,3},
+						   {4,0,0,8,0,3,0,0,1},
+						   {7,0,0,0,2,0,0,0,6},
+						   {0,6,0,0,0,0,2,8,0},
+						   {0,0,0,4,1,9,0,0,5},
+						   {0,0,0,0,8,0,0,7,9}};
+		Sudoku sdku = new Sudoku(matrix);
+		boolean found = sdku.isNumberExistedInSquare(3, 3, 9);
+		System.out.println(found ? "found" : "not found");
+			
 	}
 
 }
